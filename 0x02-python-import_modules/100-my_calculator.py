5ee27b088a0d0ac6865733b0e38d8e42ac191c5b
kinzydwayne <kinzydwaynecarter@gmail.com>
2022-09-27 04:30:04 -0700

    Build my own calculator!

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,32 @@
 #!/usr/bin/python3
+
+from sys import argv, exit as sys_exit
+
 from calculator_1 import add, sub, mul, div
 
 
-def arg_calc(argv):
-    n = len(argv) - 1
-    if n != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    a = int(argv[1])
-    operator = argv[2]
-    b = int(argv[3])
-    if operator == '+':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, add(a, b)))
-    elif operator == '-':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, sub(a, b)))
-    elif operator == '*':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, mul(a, b)))
-    elif operator == '/':
-        print("{:d} {:s} {:d} = {:d}".format(a, operator, b, div(a, b)))
-    else:
-        print("Unknown operatorerator. Available operators: +, -, * and /")
-        exit(1)
-
-
 if __name__ == "__main__":
-    from sys import argv
-    arg_calc(argv)
+
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+
+    try:
+        a, op, b = argv[1:]
+
+    except ValueError:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys_exit(1)
+
+        try:
+            print(a, op, b, "=", ops[op](int(a), int(b)))
+
+        except KeyError:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys_exit(1)
+
+        except ValueError:
+            print("Invalid operand. Must be an integer")
+            sys_exit(1)
+
+        except ZeroDivisionError:
+            print("Invalid operation. Cannot divide by zero")
+            sys_exit(1)
