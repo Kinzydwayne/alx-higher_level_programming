8db93a5815051501963c81b2fc3fb431a079a728
kinzydwayne <kinzydwaynecarter@gmail.com>
2022-09-30 01:40:52 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
 
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
 
-def _reduce(fn, seq, init=0):
-    """ Reduce a list to a single value
-    """
-    try:
-        return _reduce(fn, seq[1:], fn(init, seq[0]))
-    except IndexError:
-        return init
+    return (max_list - to_sub)
 
 
 def roman_to_int(roman_string):
-    """ Convert a Roman numeral to an integer
-    """
-    n2n = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-        }
-        try:
-            numbers = list(map(n2n.get, roman_string[::-1]))
-            integer = numbers[0]
-        except (IndexError, KeyError, TypeError):
-            return 0
+    if not roman_string:
+        return 0
 
-        for i in range(len(numbers[1:])):
-            if numbers[i + 1] >= numbers[i]:
-                integer += numbers[i + 1]
-            else:
-                integer -= numbers[i + 1]
+    if not isinstance(roman_string, str):
+        return 0
 
-                return integer
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
+    for ch in roman_string:
+        for r_num in list_keys:
+            if r_num == ch:
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
+                else:
+                    list_num.append(rom_n.get(ch))
+
+                last_rom = rom_n.get(ch)
+
+        num += to_subtract(list_num)
+
+        return (num)
